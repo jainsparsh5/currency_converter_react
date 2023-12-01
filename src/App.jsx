a40c6f6c2b0c6dc54007a4301da6fde29770529c
SparshJain <jainsparsh17@gmail.com>
2023-12-01 13:09:41 +0530

    vite project created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Date: 2021/8/14
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center bg-orange-500">Currency Converter APP</h1>
+    </>
+  )
+}
+
+export default App
